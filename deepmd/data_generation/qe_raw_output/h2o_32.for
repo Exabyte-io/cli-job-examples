12213d29014cfc7f9f2036b40ea996461224b935
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2023-12-29 17:30:50 +0800

    SOF-7178: update quantum espresso input and output files

--- deepmd/data_generation/qe_raw_output/h2o_32.for
+++ deepmd/data_generation/qe_raw_output/h2o_32.for
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0413eeff72eac026c81fec79a4cf1db4b616dc056292135b553cb000500c6c8
-size 73160
+oid sha256:245683b8f5949ca4ee10c351d506a32f71f3791e3fc706c32475caede79e0743
+size 731900
