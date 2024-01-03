66707576a436a6197021c73fcd117fb05dda7f66
Timur Bazhirov <timur@exabyte.io>
2024-01-03 02:23:37 -0800

    chore: add debug output

--- deepmd/train_mlff_qe-cp-traj/run.sh
+++ deepmd/train_mlff_qe-cp-traj/run.sh
@@ -11,35 +11,36 @@
 set_dependency_for_file_on_job_id() {
     FILE=$1
     JOB_ID=$2
-    sed -i "s/##PBS -W/#PBS -W/g" $FILE
-    sed -i "s/JOB_ID_TO_WAIT_FOR/${JOB_ID}/g" $FILE
+    sed "s/##PBS -W/#PBS -W/g" $FILE > "$FILE.with_dependency"
+    sed -i "s/JOB_ID_TO_WAIT_FOR/${JOB_ID}/g" "$FILE.with_dependency"
 }
 
 echo "Submitting the job for step_01"
 
 cd step_01*
 JOB_ID_STEP_01=$(qsub job.pbs)
-cd -
+cd - &> /dev/null
 
 echo "Submitting the job for step_02"
 cd step_02*
     set_dependency_for_file_on_job_id job.pbs $JOB_ID_STEP_01
-    JOB_ID_STEP_02=$(qsub job.pbs)
-cd -
+    JOB_ID_STEP_02=$(qsub job.pbs.with_dependency)
+cd - &> /dev/null
 
 echo "Submitting the job for step_03"
 cd step_03*
     set_dependency_for_file_on_job_id job.pbs $JOB_ID_STEP_02
-    JOB_ID_STEP_03=$(qsub job.pbs)
-cd -
+    JOB_ID_STEP_03=$(qsub job.pbs.with_dependency)
+# cd back to the root directory
+cd - &> /dev/null
 
 echo "Submitting the job for step_04"
 cd step_04*
     set_dependency_for_file_on_job_id job.pbs $JOB_ID_STEP_03
-    JOB_ID_STEP_04=$(qsub job.pbs)
-cd -
+    JOB_ID_STEP_04=$(qsub job.pbs.with_dependency)
+cd - &> /dev/null
 
-echo "Finished submitting jobs."
+echo "Finished submitting jobs for $JOB_ID_STEP_01, $JOB_ID_STEP_02, $JOB_ID_STEP_03, $JOB_ID_STEP_04."
 
 echo "Waiting for the last job to finish..."
 echo "Use 'qstat' to check the status of the jobs."
